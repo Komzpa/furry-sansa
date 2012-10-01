c71a4da4ebaaa4b5c4410316684c9802d7471005
Darafei Praliaskouski <komzpa@gmail.com>
2012-10-01 18:30:29 +0300

    dump synth option

--- README.md
+++ README.md
@@ -6,11 +6,15 @@ Updater for osm2pgsql database and rendering stylesheets used by Komzpa.
 Name suggested by github project name generator :3
 
 Special thanks to:
-    Kai Krueger aka apmon, osm2pgsql maintainer who added almost all features I needed;
-    Jochen Topf, for openstreetmapdata.com source of coastlines;
-    OSM community, who created the best world map.
+ * Kai Krueger aka apmon, osm2pgsql maintainer who added almost all features I needed;
+ * Jochen Topf, for openstreetmapdata.com source of coastlines;
+ * OSM community, who created the best world map.
 
     
 Depends:
-    osm2pgsql 0.81.0+
+ * osm2pgsql 0.81.0+ https://github.com/openstreetmap/osm2pgsql
+ * osm-c-tools http://gitorious.org/osm-c-tools
+ * wget
+ 
+    
     
--- furry.py
+++ furry.py
@@ -29,7 +29,6 @@ logger.debug('started with options: '+ ' '.join(sys.argv))
 def execute(commandline):
     """
     A method that calls the coomandline, logs properly and measures exectution time.
-    
     """
     time = datetime.datetime.now()
     logger.info('starting "%s"'% commandline)
@@ -44,9 +43,26 @@ def execute(commandline):
 
 action = sys.argv[2]
 
+if not os.path.exists(instance['tmpdir']):
+    logger.info('creating temp directory %s'%instance['tmpdir'])
+    os.makedirs(instance['tmpdir'])
+
 if action == 'import':
     logger.debug('starting osm2pgsql import')
     execute('osm2pgsql ' +osm2pgsql_actions['create'])
+    # TODO: create index
+
+elif action == 'synthdump':
+    logger.debug('starting dump synthesis')
+    logger.info('downloading dumps')
+    os.chdir(instance['tmpdir'])
+    for name, url in instance['external_dumps']:
+        execute('wget -c -O "%s" "%s"'%(name, url))
+
+    logger.info('merging final dump')
+    local_filenames = '"' + '" "'.join([i[0] for i in instance['external_dumps']]) + '"'
+    execute('osmconvert %s > %s'%( local_filenames, instance['dump'] ))
+    
 else:
     logger.error('unknown action %s'%action)
     exit(10)
