de1cb7853c01d3f68362778b7d33ae0bc113947f
Darafei Praliaskouski <komzpa@gmail.com>
2012-10-02 18:46:44 +0300

    update actions

--- furry.py
+++ furry.py
@@ -27,18 +27,23 @@ logger.setLevel(logging.DEBUG)
 logger.debug('started with options: '+ ' '.join(sys.argv))
 
 
-def execute(commandline):
+def execute(commandline, need_output = False):
     """
     A method that calls the commandline, logs properly and measures exectution time.
     """
     time = datetime.datetime.now()
     logger.info('starting "%s"'% commandline)
-    process = subprocess.Popen(commandline, bufsize=1, shell=True)
+    if need_output:
+        process = subprocess.Popen(commandline, bufsize=1, stdout=subprocess.PIPE, shell=True)
+    else:
+        process = subprocess.Popen(commandline, bufsize=1, shell=True)
     process.wait()
     if process.returncode:
         logger.error('[%s] failed "%s" in %s'%(process.returncode, commandline, str(datetime.datetime.now() - time)))
     else:
         logger.info('[%s] completed "%s" in %s'%(process.returncode, commandline, str(datetime.datetime.now() - time)))
+    if need_output:
+        return process.stdout.read()
     return process.returncode
 
 
@@ -62,18 +67,21 @@ if not os.path.exists(instance['tmpdir']):
 
 if action == 'import':
     logger.debug('starting osm2pgsql import')
-    execute('osm2pgsql ' +osm2pgsql_actions['create'])
+    if 0 == execute('osm2pgsql ' +osm2pgsql_actions['create']):
+        execute("osmconvert --out-timestamp %s > %s "%(instance['dump'],instance['pg_timestamp']))
+        if "(" in open(instance['pg_timestamp']).read():
+            # recalculating timestamp
+            os.remove(instance['pg_timestamp'])
+            timestamp = execute('osmconvert --out-statistics %s | grep "timestamp max"'%(instance['dump']), need_output = True)
+            open(instance['pg_timestamp'], 'w').write(timestamp.strip()[:11]+"00:00:00Z")
 
 elif action == "index":
     logger.debug('starting database index')
     logger.debug('preparing queries from %s' % instance['index_template'])
     queries = [line.split('-- ')[0].strip().replace('%prefix%', instance['pg_table_prefix']) for line in open(instance['index_template']) if line.split('-- ')[0].strip()]
     logger.debug('loaded %s queries, connecting to %s' % (len(queries), pg_database))
-
-
     for query in queries:
         sql(query)
-
 
 elif action == 'synthdump':
     logger.debug('starting dump synthesis')
@@ -83,16 +91,58 @@ elif action == 'synthdump':
     for name, url in instance['external_dumps']:
         if 0 == execute('wget -c -O "%s" "%s"'%(name, url)): # downloading file finished well
             if not os.path.exists(name+'.o5m'):
-                execute('osmconvert --out-o5m "%s" > "%s"'%( name, name+'.o5m'))
-                local_filenames += ' "%s.o5m"'%name
+                execute('osmconvert --out-o5m "%s" |gzip > "%s"'%( name, name+'.o5m.gz'))
+                local_filenames += ' "%s.o5m.gz"'%name
 
     logger.info('merging final dump')
-    execute('osmconvert --out-pbf %s > %s'%( local_filenames, instance['dump'] ))
+    execute('osmconvert --out-o5m %s | gzip > %s'%( local_filenames, 'merged.o5m.gz' ))
+
+    logger.info('updating dump')
+    execute('osmupdate %s %s'%( 'merged.o5m.gz', instance['dump'] ))
+
+elif action == 'updatedump':
+    logger.info('updating dump')
+    if os.path.exists(instance['cumulative_diff']):
+        if 0 == execute("osmconvert --out-pbf %s %s > %s.new"%(instance['dump'], instance['cumulative_diff'], instance['dump'])):
+            os.remove(instance['dump'])
+            os.rename(instance['dump']+".new", instance['dump'])
+    else:
+        tmpfile = os.path.join(instance['tmpdir'], 'newdump.osm.pbf') )
+        if 0 == execute('osmupdate %s %s'%( instance['dump'], tmpfile):
+            os.remove(instance['dump'])
+            os.rename(tmpfile, instance['dump'])
 
 elif action == 'getdiff':
-    # TODO
-    pass
-    
+    logger.debug('getting diffs')
+    try:
+        timestamp = open(instance['pg_timestamp']).read().strip()
+    except:
+        logger.error('can not read timestamp file %s, please fill it!'%instance['pg_timestamp'])
+        exit(1)
+    if os.path.exists(instance['current_diff']):
+        timestamp_diff = execute('osmconvert --out-timestamp "%s"'%instance['current_diff'] , need_output = True)
+        if timestamp_diff == timestamp:
+            os.remove(instance['current_diff'])
+    if not os.path.exists(instance['current_diff']):
+        logger.debug('getting new diffs')
+        execute('osmupdate %s %s'%(timestamp, instance['current_diff']))
+        timestamp_diff = execute('osmconvert --out-timestamp "%s"'%instance['current_diff'] , need_output = True)
+        logger.debug('got new diffs, old timestamp %s, new %s'%(timestamp, timestamp_diff))
+    #updating cumulative diff
+    if os.path.exists(instance['current_diff']):
+        if not os.path.exists(instance['cumulative_diff']):
+            execute("osmconvert --out-o5c %s > %s"%(instance['current_diff'], instance['cumulative_diff']))
+        else:
+            if 0 == execute("osmconvert --merge-versions --out-o5c %s %s > %s.new" % (instance['cumulative_diff'], instance['current_diff'], instance['cumulative_diff'])):
+                os.remove(instance['cumulative_diff'])
+                os.rename(instance['cumulative_diff']+".new", instance['cumulative_diff'])
+
+elif action == 'diff2db':
+    logger.debug('applying diffs to db')
+    if 0 == execute('osm2pgsql ' +osm2pgsql_actions['append']):
+        execute("osmconvert --out-timestamp %s > %s "%(instance['current_diff'],instance['pg_timestamp']))
+        
+
 else:
     logger.error('unknown action %s'%action)
     exit(10)
