e11c38efb8f1f0e4f97da21999f35ce02528c6a5
Darafei Praliaskouski <komzpa@gmail.com>
2012-10-02 12:17:02 +0300

    'index' action that creates all the indexes on the database

--- furry.py
+++ furry.py
@@ -3,6 +3,7 @@ import sys
 import logging
 import datetime
 import subprocess
+import psycopg2
 
 
 if len(sys.argv) < 3:
@@ -28,7 +29,7 @@ logger.debug('started with options: '+ ' '.join(sys.argv))
 
 def execute(commandline):
     """
-    A method that calls the coomandline, logs properly and measures exectution time.
+    A method that calls the commandline, logs properly and measures exectution time.
     """
     time = datetime.datetime.now()
     logger.info('starting "%s"'% commandline)
@@ -39,7 +40,16 @@ def execute(commandline):
     else:
         logger.info('[%s] completed "%s" in %s'%(process.returncode, commandline, str(datetime.datetime.now() - time)))
     return process.returncode
-    
+
+
+def sql(cursor, query):
+    """
+    A wrapper that measures query execution time.
+    """
+    time = datetime.datetime.now()
+    logger.info('executing query [%s]'% query)
+    cursor.execute(query)
+    logger.info('[%s] completed in %s'%(query, str(datetime.datetime.now() - time)))
 
 action = sys.argv[2]
 
@@ -50,7 +60,19 @@ if not os.path.exists(instance['tmpdir']):
 if action == 'import':
     logger.debug('starting osm2pgsql import')
     execute('osm2pgsql ' +osm2pgsql_actions['create'])
-    # TODO: create index
+
+elif action == "index":
+    logger.debug('starting database index')
+    logger.debug('preparing queries from %s' % instance['index_template'])
+    queries = [line.split('-- ')[0].strip().replace('%prefix%', instance['pg_table_prefix']) for line in open(instance['index_template']) if line.split('-- ')[0].strip()]
+    logger.debug('loaded %s queries, connecting to %s' % (len(queries), pg_database))
+    database_connection = psycopg2.connect(pg_database)
+    database_connection.set_isolation_level(0) # now we don't have to COMMIT everything
+    database_cursor = database_connection.cursor()
+
+    for query in queries:
+        sql(database_cursor, query)
+
 
 elif action == 'synthdump':
     logger.debug('starting dump synthesis')
@@ -60,12 +82,16 @@ elif action == 'synthdump':
     for name, url in instance['external_dumps']:
         if 0 == execute('wget -c -O "%s" "%s"'%(name, url)): # downloading file finished well
             if not os.path.exists(name+'.o5m'):
-                execute('osmconvert --out-o5m "%s" > %s'%( name, name+'.o5m'))
+                execute('osmconvert --out-o5m "%s" > "%s"'%( name, name+'.o5m'))
                 local_filenames += ' "%s.o5m"'%name
 
     logger.info('merging final dump')
     execute('osmconvert --out-pbf %s > %s'%( local_filenames, instance['dump'] ))
 
+elif action == 'getdiff':
+    # TODO
+    pass
+    
 else:
     logger.error('unknown action %s'%action)
     exit(10)
